a360f7da9084f3e6a1ec7d23df84dfb1b2d93098
phale20202007@gmail.com <phale20202007@gmail.com@a1bde17d-a4f8-77e8-c7c2-a8fb63126ac9>
2013-10-01 18:26:24 +0000

    code xong bài 3, chưa chỉnh giao diện nhờ Q chỉnh sửa lại giao diện giúp Y :)

---
+++ KhaiThacDuLieu/BTT03/PhanLopDuLieu/TienXyLyDuLieu/Item.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhanLop
+{
+    public class Item
+    {
+        private int _index;
+
+        public int Index
+        {
+            get { return _index; }
+            set { _index = value; }
+        }
+        private double _distance;
+
+        public double Distance
+        {
+            get { return _distance; }
+            set { _distance = value; }
+        }
+
+        private string _attributeTOP;
+
+        public string AttributeTOP
+        {
+            get { return _attributeTOP; }
+            set { _attributeTOP = value; }
+        }
+        public void Init(Item x)
+        {
+            this.Index = x.Index;
+            this.Distance = x.Distance;
+            this.AttributeTOP = x.AttributeTOP;
+        }
+    }
+}
