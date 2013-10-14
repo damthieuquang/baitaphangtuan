a04531b9c0a4fe3246791757ab35faf0128564e5
damthieuquang@gmail.com <damthieuquang@gmail.com@a1bde17d-a4f8-77e8-c7c2-a8fb63126ac9>
2013-10-14 14:53:38 +0000

    Bài tập 4

---
+++ trunk/KhaiThacDuLieu/BTT04/BaiTap04/BaiTap04/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace BaiTap04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_ClusterOuput = new System.Windows.Forms.TextBox();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton_classes_to_cluster = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txt_NumK = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_ClusterOuput);
+            this.groupBox1.Location = new System.Drawing.Point(294, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(461, 428);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cluster output";
+            // 
+            // txt_ClusterOuput
+            // 
+            this.txt_ClusterOuput.Location = new System.Drawing.Point(6, 19);
+            this.txt_ClusterOuput.Multiline = true;
+            this.txt_ClusterOuput.Name = "txt_ClusterOuput";
+            this.txt_ClusterOuput.Size = new System.Drawing.Size(449, 403);
+            this.txt_ClusterOuput.TabIndex = 0;
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Location = new System.Drawing.Point(16, 19);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(75, 23);
+            this.btn_Open.TabIndex = 1;
+            this.btn_Open.Text = "Open";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button_Start);
+            this.groupBox2.Controls.Add(this.btn_Open);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(736, 55);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cluster";
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(124, 19);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(75, 23);
+            this.button_Start.TabIndex = 2;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton_classes_to_cluster);
+            this.groupBox3.Location = new System.Drawing.Point(12, 73);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(276, 51);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Cluster Mode";
+            // 
+            // radioButton_classes_to_cluster
+            // 
+            this.radioButton_classes_to_cluster.AutoSize = true;
+            this.radioButton_classes_to_cluster.Location = new System.Drawing.Point(16, 20);
+            this.radioButton_classes_to_cluster.Name = "radioButton_classes_to_cluster";
+            this.radioButton_classes_to_cluster.Size = new System.Drawing.Size(107, 17);
+            this.radioButton_classes_to_cluster.TabIndex = 0;
+            this.radioButton_classes_to_cluster.TabStop = true;
+            this.radioButton_classes_to_cluster.Text = "Classes to cluster";
+            this.radioButton_classes_to_cluster.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Controls.Add(this.txt_NumK);
+            this.groupBox4.Location = new System.Drawing.Point(12, 131);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(276, 364);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "groupBox4";
+            // 
+            // txt_NumK
+            // 
+            this.txt_NumK.Location = new System.Drawing.Point(64, 22);
+            this.txt_NumK.Name = "txt_NumK";
+            this.txt_NumK.Size = new System.Drawing.Size(100, 20);
+            this.txt_NumK.TabIndex = 0;
+            this.txt_NumK.Text = "2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "K";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 513);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_ClusterOuput;
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton_classes_to_cluster;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_NumK;
+    }
+}
+
---
+++ trunk/KhaiThacDuLieu/BTT04/BaiTap04/BaiTap04/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BaiTap04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Dung de chua du lieu doc tu file
+        private static DataSet ds = new DataSet();
+        //Mac dinh k se bang 2
+        private static int k = 2;
+
+        /************************************************************************/
+        /* Doc file va luu vao dataset                                                                     */
+        /************************************************************************/
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "(*.arff)|*.arff";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                string path = dlg.FileName;
+                StreamReader reader = new StreamReader(path);
+                //Khai bao table trong dataset 
+                ds.Tables.Add("data");
+
+                //Doc dong dau tien
+                string str = reader.ReadLine();
+
+                do
+                {
+                    str = reader.ReadLine();
+                } while (str.IndexOf('@') != 0);//Kiem tra xem dong do co chua ki tu @
+
+                do
+                {
+                    str = reader.ReadLine();
+                    //Neu dong do ton tai khoang trang va @
+                    if (str.Contains('@') && str.Contains(' '))
+                    {
+                        //Thuc hien cat chuoi
+                        String[] title = str.Split(' ');
+                        //Dua tieu de vao dataset
+                        ds.Tables["data"].Columns.Add(title[1]);
+                    }
+                } while (str.Length == 0 || str.Contains(' '));
+
+                //Doc den het file
+                while (!reader.EndOfStream)
+                {
+                    str = reader.ReadLine();
+                    if (str.Contains(','))
+                    {
+                        String[] items = str.Split(",".ToCharArray());
+                        ds.Tables["data"].Rows.Add(items);
+                    }
+                }
+                reader.Close();
+                //DataRow dr = ds.Tables["data"].Rows[0];
+
+                //textBox_ClusterOuput.Text = dr[0].ToString();
+                //dataGridView1.DataSource = ds.Tables["data"].DefaultView;
+            }
+        }
+        /************************************************************************/
+        /* Tinh khoang cach giua 2 diem du lieu                                                                     */
+        /************************************************************************/
+        private double DistanceMinkowski(DataRow dr1, DataRow dr2, int q)
+        {
+            double result = -1;
+            //Lay so cot
+            int numcol = ds.Tables["data"].Columns.Count;
+
+            for (int i = 0; i < numcol; i++)
+            {
+                double out1, out2;
+
+                //Chuyen thanh kieu double
+                out1 = double.Parse(dr1[i].ToString());
+                out2 = double.Parse(dr2[i].ToString());
+
+                result += Math.Pow(out1 - out2, q);//Truong hop q = 1 la Manhattan, q = 2 la Euclide, q > 2 Minkowski
+            }
+            //Lay can bat q
+            result = Math.Pow(result, 1 / (q * 1.0));
+
+            return result;
+        }
+
+        /************************************************************************/
+        /* Kiem tra du lieu co ton tai hay khong                                                                     */
+        /************************************************************************/
+        private bool CheckData()
+        {
+            bool result = true;
+
+            if (ds.Tables["data"] == null)
+                result = false;
+
+            return result;
+        }
+        /************************************************************************/
+        /* Phan lop du lieu                                                                     */
+        /************************************************************************/
+        private void Cluster()
+        {
+            //Lay so cum
+            k = int.Parse(txt_NumK.ToString());
+
+            //Lay gia tri ngau nhien
+            DataRow k1, k2, k3;
+            k1 = ds.Tables["data"].Rows[0];
+            k2 = ds.Tables["data"].Rows[1];
+            k3 = ds.Tables["data"].Rows[2];
+
+            //Duyet het tung dong trong du lieu
+            foreach (DataRow datarow in ds.Tables["data"].Rows)
+            {
+                double rs1, rs2, rs3;
+                //Tinh khoang cach
+                rs1 = DistanceMinkowski(k1, datarow, 2);
+                rs2 = DistanceMinkowski(k2, datarow, 2);
+                rs3 = DistanceMinkowski(k3, datarow, 2);
+
+                
+            }
+        }
+
+        /************************************************************************/
+        /* Thuc hien tinh K-Means                                                                     */
+        /************************************************************************/
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            //Kiem tra du lieu
+            if (!CheckData())
+            {
+                MessageBox.Show("Chưa nhập dữ liệu!");
+                return;
+            }
+            
+        }
+    }
+}
